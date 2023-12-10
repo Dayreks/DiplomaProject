039c0ee015df7ee41a120ee24af4f095a9803909
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2023-12-10 19:53:54 +0100

    Initial Commit

---
+++ DiplomaProject/DiplomaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomaProjectApp.swift
+//  DiplomaProject
+//
+//  Created by Bohdan Arkhypchuk on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiplomaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
