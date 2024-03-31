1ffc56aac4ece50485d53fb9300f550accde3ca0
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2024-03-31 15:06:54 +0200

    added macro builder for setters

--- CodeGenerator/Builders/VariableMacroBuilder.swift
+++ CodeGenerator/Builders/VariableMacroBuilder.swift
@@ -62,18 +62,51 @@ class VariableMacroBuilder: Builder {
                 break
             }
             
-            let propertyName = identifier.identifier.text
-            let propertyType = binding.typeAnnotation?.type.description ?? "Type"
-            
             let funcVisitor = try FuncVisitor(source: outputCode)
-            let methodName = funcitonSyntax.name
-            let methodParameters = funcitonSyntax.signature.parameterClause.parameters.map { ($0.firstName, $0.secondName, $0.type)}
-            let methodStatements = funcitonSyntax.body?.statements
+            var macroInsertionCode = """
+            guard let variableDecl = declaration.as(VariableDeclSyntax.self) else {
+                                    fatalError()
+            }
+                                                
+            // Extract property name and type
+            guard 
+                let binding = variableDecl.bindings.first,
+                let identifier = binding.pattern.as(IdentifierPatternSyntax.self)
+            else {
+                    fatalError()
+            }
+
+            """
             
-            if funcVisitor.declarationReferences.map({ $0.baseName.text }).contains(propertyName) {
-                var resultMethod = outputCode
-                
-                
+            
+            var propertyName = identifier.identifier.text
+            
+            if funcVisitor.declarationReferences.map({ $0.baseName.text }).contains(identifier.identifier.text) && funcVisitor.declarationReferences.map({ $0.baseName.text }).contains("self") {
+                macroInsertionCode.append("\nlet propertyName = identifier.identifier.text")
+                propertyName = "\\(propertyName)"
+            }
+            
+            var methodName = funcitonSyntax.name.text.lowercased()
+            if methodName.contains(identifier.identifier.text) {
+                methodName = methodName.replacingOccurrences(of: identifier.identifier.text, with: propertyName)
+                macroInsertionCode.append("\nlet methodName = \"\(methodName)\"")
+                methodName = "\\(methodName)"
+            }
+            
+            let methodParameters = funcitonSyntax.signature.parameterClause.parameters.map { ($0.firstName, $0.secondName, $0.type)}
+            
+            var propertyType = binding.typeAnnotation?.type.description.trimmingTrailingWhitespace() ?? "Type"
+            if
+                let methodParameter = methodParameters
+                    .first(where: { parameter in
+                        funcVisitor.declarationReferences.map({ $0.baseName.text }).contains(where: { varDeclararion in
+                            parameter.0.text == varDeclararion || parameter.1?.text == varDeclararion
+                        })
+                    }),
+                methodParameter.2.description == propertyType
+            {
+                macroInsertionCode.append("\nlet propertyType = binding.typeAnnotation?.type.description ?? \"Type\"")
+                propertyType = "\\(propertyType)"
             }
             
             return """
@@ -87,21 +120,8 @@ class VariableMacroBuilder: Builder {
                     providingPeersOf declaration: some DeclSyntaxProtocol,
                     in context: some MacroExpansionContext
                 ) throws -> [DeclSyntax]  {
-                    guard let variableDecl = declaration.as(VariableDeclSyntax.self) else {
-                        throw MacroError.notVariable
-                    }
-                                    
-                    // Extract property name and type
-                    guard let binding = variableDecl.bindings.first,
-                    let identifier = binding.pattern.as(IdentifierPatternSyntax.self) else {
-                        throw MacroError.notValidIdentifier
-                    }
-
-                    let propertyName = identifier.identifier.text
-                    let propertyType = binding.typeAnnotation?.type.description ?? "Type"
-
-                    // Generate the setter method code
-                    let methodName = "set\(propertyName.capitalized)"
+                    \(macroInsertionCode)
+            
                     let methodCode = \"\"\"
                     func \(methodName)(_ newValue: \(propertyType)) {
                         self.\(propertyName) = newValue
