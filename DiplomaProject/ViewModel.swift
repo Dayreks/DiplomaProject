c646be4e21fea5dcdeb9f5f318d60a627f51a939
Bohdan Arkhypchuk <bohdan.arkhypchuk@gen.tech>
2024-04-07 22:17:59 +0200

    trimming fix

--- DiplomaProject/ViewModel.swift
+++ DiplomaProject/ViewModel.swift
@@ -23,8 +23,8 @@ final class ViewModel: ObservableObject {
     private var defaults = UserDefaults(suiteName: "524636QW8M.group.com.bohdanarkhypchuk.ukma.ua.DiplomaProject")
     
     init() {
-        inputCode =  defaults?.string(forKey: StorageKeys.inputCode)?.trimmingTrailingWhitespace() ?? ""
-        outputCode =  defaults?.string(forKey: StorageKeys.outputCode)?.trimmingTrailingWhitespace() ?? ""
+        inputCode =  defaults?.string(forKey: StorageKeys.inputCode)?.trimmingCharacters(in: [" ", "\n"]) ?? ""
+        outputCode =  defaults?.string(forKey: StorageKeys.outputCode)?.trimmingCharacters(in: [" ", "\n"]) ?? ""
         isOutputBlocked = defaults?.bool(forKey: StorageKeys.isOutputBlocked) ?? false
     }
     
